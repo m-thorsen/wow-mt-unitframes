eb824f873f90cc54e7eed9b7e47b88ebe91dc46c
Martin <m@codedrecordings.com>
2020-12-20 19:41:01 +0100

    ?

--- setup.lua
+++ setup.lua
@@ -26,3 +26,53 @@ function MTUnitFrames:ChatCommand()
     InterfaceOptionsFrame_OpenToCategory(self.optionsFrame);
     InterfaceOptionsFrame_OpenToCategory(self.optionsFrame); -- Intentionally called twice due to a bliz bug
 end;
+
+-- Add Interface option
+function MTUnitFrames:GetOptions()
+    return {
+        name = "MTUnitFrames",
+        handler = MTUnitFrames,
+        type = "group",
+        args = {
+            11 = {
+                order = 11,
+                type = "range",
+                name = "Space between",
+                min = 100,
+                max = 500,
+                step = 1,
+                get = function(info)
+                    return self.db.global.offsetX;
+                end,
+                set = function(info, value)
+                    self.db.global.offsetX = value;
+                    self:InitMoveFrames();
+                end,
+            },
+            ab = {
+                order = 12,
+                type = "range",
+                name = "Distance from bottom",
+                min = 150,
+                max = 300,
+                step = 1,
+                get = function(info)
+                    return self.db.global.offsetY;
+                end,
+                set = function(info, value)
+                    self.db.global.offsetY = value;
+                    self:InitMoveFrames();
+                end,
+            },
+            ac = {
+                order = 13,
+                type = "execute",
+                name = "Reset to defaults",
+                func = function()
+                    self.db:ResetDB();
+                    ReloadUI();
+                end,
+            },
+        },
+    };
+end;
